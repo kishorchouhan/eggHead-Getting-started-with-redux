fd14864f1f8bc4a19dc05daa36f0f68e8679c4bd
kishorchouhan <devkishorbwr@gmail.com>
2019-07-12 20:09:54 +0530

    L11 done.

--- 10-avoiding-object-mutations-with-object-assign-and-spread/script.js
+++ 10-avoiding-object-mutations-with-object-assign-and-spread/script.js
@@ -3,7 +3,7 @@
  * that the tests have passed.
  */
 
-const toggleTodo = (todo) => {
+const toggleTodo = todo => {
   return {
     ...todo,
     completed: !todo.completed
@@ -13,42 +13,38 @@ const toggleTodo = (todo) => {
 const testToggleTodo = () => {
   const todoBefore = {
     id: 0,
-    text: 'Learn Redux',
+    text: "Learn Redux",
     completed: false
   };
   const todoAfter = {
     id: 0,
-    text: 'Learn Redux',
+    text: "Learn Redux",
     completed: true
   };
-  
+
   deepFreeze(todoBefore);
-  
-  expect(
-    toggleTodo(todoBefore)
-  ).toEqual(todoAfter);
+
+  expect(toggleTodo(todoBefore)).toEqual(todoAfter);
 };
 
 testToggleTodo();
-console.log('All tests passed.') || displayInPreview('All tests passed.');
-
-
+console.log("All tests passed.") || displayInPreview("All tests passed.");
 
 // display in plunker preview
 function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(string);
   newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
+  document.body.appendChild(newDiv);
 }
 
 // Function exported from deep-freeze lib
-function deepFreeze (o) {
-  if (o===Object(o)) {
-    Object.isFrozen(o) || Object.freeze(o)
-    Object.getOwnPropertyNames(o).forEach(function (prop) {
-      prop==='constructor'||deepFreeze(o[prop])
-    })
+function deepFreeze(o) {
+  if (o === Object(o)) {
+    Object.isFrozen(o) || Object.freeze(o);
+    Object.getOwnPropertyNames(o).forEach(function(prop) {
+      prop === "constructor" || deepFreeze(o[prop]);
+    });
   }
-  return o
+  return o;
 }
